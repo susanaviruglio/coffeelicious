8f482e9dff40d41da850a5cf43ba06f2c82411ab
Susana Viruglio <susanaviruglio@hotmail.com>
2024-01-05 20:14:27 +0000

    Registered Product and Category app to be able to see it in the system

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+# I have registered Product and Category to be able to see it 
+admin.site.register(Product)
+admin.site.register(Category)
