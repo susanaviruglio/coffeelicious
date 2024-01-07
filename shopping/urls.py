2898d5610a6388daeb8fabd04e6b2a8c94b9ee9e
Susana Viruglio <susanaviruglio@hotmail.com>
2024-01-07 17:47:16 +0000

    added view.py shopping bag functionality to add item

--- shopping/urls.py
+++ shopping/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.shopping_bag, name='shopping_bag') # path to shopping bag 
+    path('', views.shopping_bag, name='shopping_bag'), # path to shopping bag 
+    path('add/<item_id>/', views.add_to_shopping_bag, name='add_to_shopping_bag'), # path to add items
 ]
--- shopping/views.py
+++ shopping/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def shopping_bag(request):
     """A view to return the shopping bag """
     
     return render(request, 'shopping/shopping.html')
+
+
+def add_to_shopping_bag(request, item_id):
+    """ to add times to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    # turn into an int so it the function does not return a string
+    redirect_url = request.POST.get('redirect_url')
+    # to redirect the user to the previous url when an item has been purchased
+
+    bag = request.session.get('bag', {})
+    # this variable allows items to be store while the user keeps shopping
+
+    if item_id in list(bag.keys()):
+       
+        bag[item_id] += quantity
+        # if there's already a product then increment the quantity
+    else:
+        bag[item_id] = quantity
+        # item id is equal to the quantity
+
+    request.session['bag'] = bag
+    # add the session again with the updated item
+
+    return redirect(redirect_url)
