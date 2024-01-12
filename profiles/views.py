5c758294d276ff14062d009dea9771a369038877
Susana Viruglio <susanaviruglio@hotmail.com>
2024-01-12 22:42:43 +0000

    modified allauth templates and tested

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
 
 # a view of the profile 
 def profile(request):
     """ Display the user's profile. """
-
+    profile = get_object_or_404(UserProfile, user=request.user)
+    # if there is any error then show error 404
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
