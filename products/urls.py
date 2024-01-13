0fc25004d479b48ac76a003b95c58b8c53d81cde
Susana Viruglio <susanaviruglio@hotmail.com>
2024-01-13 17:43:20 +0000

    created edit product template, added to views and urls

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:product_id>/', views.product_description, name='product_description'), 
     # product description file path, it should be an integer
     path('add/', views.add_product, name='add_product'), # path for adding more products to the store
-]
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'), # path for editing products
+    ]
--- products/views.py
+++ products/views.py
@@ -129,3 +129,34 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+# to be able to edit the products in the store
+def edit_product(request, product_id):
+    """ Edit a product in the store """
+    product = get_object_or_404(Product, pk=product_id)
+    # if there is any error then show error
+    if request.method == 'POST':
+        # if the request is post then get the files 
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            # if the form is valid then save and show a message
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_description', args=[product.id]))
+        else:
+            # otherwise it will add a fail statement
+            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+    else:
+        # message info to edit a product
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    # template directory
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
