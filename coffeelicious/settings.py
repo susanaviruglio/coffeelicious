673e352bec35b4c69618e81294f52b85acacc30b
Susana Viruglio <susanaviruglio@hotmail.com>
2024-01-13 23:09:24 +0000

    modified debug and secret_key

--- coffeelicious/settings.py
+++ coffeelicious/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q)eopmi%)2j9ff=-jvz_2itr7=$^qqc^&q#&#$me#ih6^cv+7^'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['coffeelicious-0a5548d626d6.herokuapp.com', 'localhost'] # for heroku
 
