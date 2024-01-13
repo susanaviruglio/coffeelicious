21a35edc477d01cc8b3fb5505528d56e788d9158
Susana Viruglio <susanaviruglio@hotmail.com>
2024-01-13 18:47:56 +0000

    fixing the image field and added django widget template!

--- products/forms.py
+++ products/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Product, Category
 
 # allow superuser the ability to add and delete forms
@@ -7,6 +8,8 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product 
         fields = '__all__' # select all the fields
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
